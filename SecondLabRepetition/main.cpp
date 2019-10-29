d61a2c0035f8a9d2ae071878178f0c36ac3b770d
Col1nn <pepito98@tlen.pl>
2019-10-29 11:40:00 +0100

    Initial Commit

---
+++ SecondLabRepetition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SecondLabRepetition
+//
+//  Created by Mateusz Kot on 29/10/2019.
+//  Copyright © 2019 Mateusz Kot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
